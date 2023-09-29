2d29da2d44320ee6cc2a8861a30eb2aca253b7a1
Patryk Skwara <patryk.skwara750@gmail.com>
2023-09-29 09:38:34 +0200

    netlify

--- src/redux/thunks.ts
+++ src/redux/thunks.ts
@@ -4,13 +4,14 @@ import axios, {AxiosError} from "axios";
 import {ExamType, Questions} from "../types";
 import {PayloadAction} from "@reduxjs/toolkit";
 
+const URL: string = process.env.REACT_APP_REALTIME_DATABASE!
 
 export const fetchExam = (examType: ExamType | string) => {
     return async (dispatch: Dispatch) => {
         dispatch(actions.loading())
 
         const response: PayloadAction<{ questions: Questions }, "exam/startExam"> | PayloadAction<{ error: AxiosError }, "exam/catchException"> =
-            await axios.get(`${process.env.REACT_APP_REALTIME_DATABASE}${examType}.json`)
+            await axios.get(`${URL}${examType}.json`)
                 .then((response) => dispatch(actions.startExam({questions: response.data})))
                 .catch((error: AxiosError) => dispatch(actions.catchException({error})));
 
@@ -23,7 +24,7 @@ export const fetchExamKeywords = () => {
         dispatch(actions.loading())
 
         const response: PayloadAction<{ exams: string[] }, "exam/chooseExam"> | PayloadAction<{ error: AxiosError }, "exam/catchException"> =
-            await axios.get(`${process.env.REACT_APP_REALTIME_DATABASE}ALL.json`)
+            await axios.get(`${URL}ALL.json`)
                 .then((response) => dispatch(actions.chooseExam({exams: response.data})))
                 .catch((error: AxiosError) => dispatch(actions.catchException({error})))
 
