8702fc2f75246add9ea156ea35cbbe43405e261d
Patryk Skwara <patryk.skwara750@gmail.com>
2023-11-15 11:02:02 +0100

    add button added

--- src/components/manageExams/ManageExamsPage.tsx
+++ src/components/manageExams/ManageExamsPage.tsx
@@ -1,9 +1,18 @@
 import {DataTable} from "../../ui/datatable/DataTable";
+import classes from "./ManageExamsPage.module.scss"
+import {Button} from "../../ui/atoms/buttons/button";
+import plusIcon from "../../ui/atoms/icons/icons8-plus-48.png"
 
 export const ManageExamsPage = () => {
 
     return (
         <div>
+            <div className={classes.add__exam}>
+                <Button>
+                    <img src={plusIcon} alt="plus_Icon" />
+                    <span>Add</span>
+                </Button>
+            </div>
             <DataTable />
         </div>
     )
